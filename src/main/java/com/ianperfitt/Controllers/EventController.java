4a6d32c457dc19192d10dc208177999755987afe
Ian Perfitt <ianperfitt@gmail.com>
2020-03-18 14:45:51 -0500

    created a PostGreSQLConfig class

--- src/main/java/com/ianperfitt/Controllers/EventController.java
+++ src/main/java/com/ianperfitt/Controllers/EventController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class EventController {
 	@Autowired
 	private EventService es;
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String hello() {
 		return "Hello";
 	}
