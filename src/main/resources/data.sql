27770cd2521e33ec7a05f01ed4232bbe9bb806ee
Ian Perfitt <ianperfitt@gmail.com>
2020-03-31 11:24:24 -0500

    created event_address table in h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,20 @@ CREATE TABLE IF NOT EXISTS event (
 	genre VARCHAR(200)
 );
 
+CREATE TABLE IF NOT EXISTS event_address (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	house_number VARCHAR(200),
+	street VARCHAR(200),
+	city VARCHAR(200),
+	state VARCHAR(200),
+	country VARCHAR(200),
+	zipcode VARCHAR(200)
+);
+
 INSERT INTO event (id,name, location, genre) VALUES
   (0,'MADDIES SHOW', 'maddies house', 'blues'),
   (1,'DADS SHOW', 'dads house', 'funk'),
   (2,'IANS SHOW', 'ians house', 'jazz');
+  
+INSERT INTO event_address (id,house_number, street, city, state, country, zipcode) VALUES
+  (0,'212', 'NW J. St.', 'Bentonville', 'Arkansas', 'US', '72712')
